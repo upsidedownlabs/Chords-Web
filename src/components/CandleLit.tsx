4197283136e6736ba9f545b883dfb21221338049
Ritika Mishra <ritikamis@gmail.com>
2025-04-23 15:36:28 +0530

    Adjust ui of flame

--- src/components/CandleLit.tsx
+++ src/components/CandleLit.tsx
@@ -48,10 +48,9 @@ const BrightCandleView: React.FC<BrightCandleViewProps> = ({ fftData = [], betaP
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 200 300"
-                    className={`absolute ${isFullPage ? '-top-[42%] sm:-top-[40%] md:-top-[50%] lg:-top-[40%]' : '-top-[25%] sm:-top-[18%] md:-top-[14%] lg:-top-[7%]'} left-1/2 transform -translate-x-1/2 w-full h-auto z-50 drop-shadow-xl pointer-events-none`}
+                    className={`absolute ${isFullPage ? '-top-[42%] sm:-top-[40%] md:-top-[50%] lg:-top-[38%]' : '-top-[25%] sm:-top-[18%] md:-top-[14%] lg:-top-[2%]'} left-1/2 transform -translate-x-1/2 w-full h-auto z-50 drop-shadow-xl pointer-events-none`}
                     preserveAspectRatio="xMidYMid meet"
                 >
-
                     <defs>
                         <linearGradient id="outerFlameGradient" x1="0%" y1="0%" x2="0%" y2="100%">
                             <stop offset="0%" stopColor={`rgba(255,140,0, ${brightness * 0.6})`} />
@@ -85,7 +84,7 @@ const BrightCandleView: React.FC<BrightCandleViewProps> = ({ fftData = [], betaP
                         className="transition-all duration-300 animate-flicker"
                     />
                 </svg>
-                <div className={`absolute bottom-0 w-full ${isFullPage ? 'h-full' : 'h-[30%] sm:h-[32%] md:h-[34%] lg:h-[36%]'} bg-gradient-to-b from-gray-100 to-gray-200 dark:from-stone-600 dark:to-stone-700  rounded-t-md  backdrop-blur-md shadow-xl before:absolute before:inset-0 before:bg-white/10 before:opacity-40 before:rounded-b-xl before:rounded-t-md`}>
+                <div className={`absolute bottom-0 w-full ${isFullPage ? 'h-full' : 'h-[30%] sm:h-[32%] md:h-[34%] lg:h-[26%]'} bg-gradient-to-b from-gray-100 to-gray-200 dark:from-stone-600 dark:to-stone-700  rounded-t-md  backdrop-blur-md shadow-xl before:absolute before:inset-0 before:bg-white/10 before:opacity-40 before:rounded-b-xl before:rounded-t-md`}>
                     <div className="absolute inset-0 overflow-hidden  rounded-t-md bg-gradient-to-b from-cyan-300 via-blue-400 to-gray-900">
                         <div className={`absolute inset-0 ${isFullPage ? 'flex flex-row justify-center pt-10' : 'flex items-center justify-center'}  `}>
                             <div className="text-sm sm:text-xl md:text-xl lg:text-3xl font-semibold text-gray-800 px-2 sm:px-3 py-1 ">
